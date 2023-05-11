9d1ea5b894541a5fd1f299d51f24501f714c6d54
rspforhp <39800618+rspforhp@users.noreply.github.com>
2023-05-11 20:57:06 +0500

    Making sdl_net and basic sdl app

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using SDLTooSharp.Bindings.SDL2;
+using SDLTooSharp.Bindings.SDL2Net;
+
+namespace MultiplayerCardGamesSimulator;
+
+public static class Extensions
+{
+    public static SDL.SDL_Rect Create(this SDL.SDL_Rect a,int x,int y,int w,int h)
+    {
+        a.H = h;
+        a.Y = y;
+        a.W = w;
+        a.X = x;
+        return a;
+    }
+}
+public static class Program
+{
+   
+    private static IntPtr _window;
+    private static IntPtr _renderer;
+    private const int ScreenWidth = 1280;
+    private const int ScreenHeight = 768;
+    public static event Action? DrawStuff;
+    private static void Main()
+    {
+        if(SDL.SDL_Init( SDL.SDL_INIT_EVERYTHING ) < 0 )
+        {
+            Console.WriteLine( $"SDL could not initialize! SDL_Error: {SDL.SDL_GetError()}\n");
+            return;
+        }
+        _window = SDL.SDL_CreateWindow( "SDL Tutorial", (int)SDL.SDL_WINDOWPOS_UNDEFINED, (int)SDL.SDL_WINDOWPOS_UNDEFINED, ScreenWidth, ScreenHeight, (uint)SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN );
+        if( _window.CompareTo(null)==0 )
+        {
+            Console.WriteLine( $"Window could not be created! SDL_Error: {SDL.SDL_GetError()}\n");
+            return;
+        }
+
+        _renderer = SDL.SDL_CreateRenderer( _window,0,0 );
+        if( _renderer.CompareTo(null)==0 )
+        {
+            Console.WriteLine( $"Renderer could not be created! SDL_Error: {SDL.SDL_GetError()}\n");
+            return;
+        }
+        DrawStuff+= delegate
+        {
+            SDL.SDL_SetRenderDrawColor(_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+            SDL.SDL_RenderFillRect( _renderer, new SDL.SDL_Rect().Create(100,100,250,250) );
+
+        };
+        
+        
+        var ip = new SDLNet.IPaddress();
+        SDLNet.SDLNet_Init();
+        SDLNet.SDLNet_ResolveHost(ref ip, "192.168.0.129", 25565);
+        Console.WriteLine(SDLNet.SDLNet_ResolveIP(in ip));
+        
+        
+        SDL.SDL_Event e; bool quit = false;
+        while (quit == false)
+        {
+            SDL.SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 0);
+            SDL.SDL_RenderClear(_renderer);
+            while (SDL.SDL_PollEvent(out e) != 0)
+            {
+                if( e.Type ==(ulong)SDL.SDL_EventType.SDL_QUIT ) quit = true;
+            }
+
+            DrawStuff?.Invoke();
+            SDL.SDL_RenderPresent(_renderer);
+
+        }
+        SDL.SDL_DestroyWindow(_window);
+        SDL.SDL_DestroyRenderer(_renderer);
+     
+        SDL.SDL_Quit();
+ 
+
+    }
+
+
+}
---
+++ SDLNet.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using SDLTooSharp.Bindings.SDL2;
+#pragma warning disable CS1591
+namespace SDLTooSharp.Bindings.SDL2Net;
+
+[ExcludeFromCodeCoverage]
+public static partial class SDLNet
+{
+    private const string dllName = "SDL2_net";
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDLNet_Linked_Version")]
+    private static extern IntPtr _SDLNet_Linked_Version();
+    public static SDL.SDL_version SDLNet_Linked_Version()
+    {
+        IntPtr version = _SDLNet_Linked_Version();
+        return Marshal.PtrToStructure<SDL.SDL_version>(version);
+    }
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int SDLNet_Init();
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int SDLNet_Quit();
+    public struct IPaddress
+    {
+        public uint host;/* 32-bit IPv4 host address */
+        public ushort port;/* 16-bit protocol port */
+    }
+    public enum INADDR : long
+    {
+        INADDR_ANY,
+        INADDR_NONE=0xFFFFFFFF,
+        INADDR_LOOPBACK=0x7f000001,
+        INADDR_BROADCAST =   0xFFFFFFFF,
+    }
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int SDLNet_ResolveHost(ref IPaddress address,[MarshalAs(UnmanagedType.BStr)] string host, ushort port );
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl,EntryPoint = "SDLNet_ResolveIP")]
+    private static extern IntPtr _SDLNet_ResolveIP(in IPaddress address);
+    public static string SDLNet_ResolveIP(in IPaddress address)=>Marshal.PtrToStringAnsi(_SDLNet_ResolveIP(address)) ?? string.Empty;
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int SDLNet_GetLocalAddresses(ref IPaddress address,int maxCount );
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr SDLNet_TCP_OpenServer(ref IPaddress address );
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr SDLNet_TCP_OpenClient(ref IPaddress address );
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr SDLNet_TCP_Open(ref IPaddress address );
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr SDLNet_TCP_Accept(IntPtr server);
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern ref IPaddress SDLNet_TCP_GetPeerAddress(IntPtr sock);
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int SDLNet_TCP_Send(IntPtr sock,in object data, int len);
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int SDLNet_TCP_Recv(IntPtr sock,ref  object data, int len);
+
+
+    
+    
+    public static int SDLNet_SetError(string error) => SDL2.SDL.SDL_SetError(error);
+    public static string SDLNet_GetError() => SDL2.SDL.SDL_GetError();
+}
